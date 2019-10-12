3dd329d35cc701ce24014aef7384f0ef12fe7ab4
Mr. Horny <akatray@gmail.com>
2019-10-12 03:28:17 +0300

    Min/Max performance based on testing.

--- stacks/OpLocal2.hpp
+++ stacks/OpLocal2.hpp
@@ -23,26 +23,25 @@ namespace sx
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	// Local dense operation 2D.
 	// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	template<Func F, u64 R, u64 IW, u64 IH, u64 OW, u64 OH> class OpLocal2 : public Op
+	template<u64 IW, u64 IH, u64 OW, u64 OH, u64 R = 1, Func F = Func::RELU> class OpLocal2 : public Op
 	{
+		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		// Compile time constants.
+		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+		constexpr static auto KER_AREA = u64(((R*2)+1)*((R*2)+1));
+		constexpr static auto KER_RDX_MIN = -i64(R);
+		constexpr static auto KER_RDX_MAX = i64(R+1);
+		constexpr static auto IN_STEP_X = r32(IW-R*2)/OW;
+		constexpr static auto IN_STEP_Y = r32(IH-R*2)/OH;
+		
 		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		// Members.
 		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		r32* OutTrans;
-		r32* OutReal;
-		r32* Gradient;
-		r32* Weights;
-		r32* WeightsDlt;
-
-		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		// constexpr magic.
-		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-		constexpr static u64 KER_AREA = ((R * 2) + 1) * ((R * 2) + 1);
-		constexpr static i64 KER_RDX_MIN = -i64(R);
-		constexpr static i64 KER_RDX_MAX = i64(R + 1);
-
-		constexpr static auto IN_STEP_X = r32(IW-R*2) / OW;
-		constexpr static auto IN_STEP_Y = r32(IH-R*2) / OH;
+		alignas(simd::ALIGNMENT) r32 OutTrans[OW*OH];
+		alignas(simd::ALIGNMENT) r32 OutReal[OW*OH];
+		alignas(simd::ALIGNMENT) r32 Gradient[IW*IH];
+		alignas(simd::ALIGNMENT) r32 Weights[OW*OH*KER_AREA];
+		alignas(simd::ALIGNMENT) r32 WeightsDlt[OW*OH*KER_AREA];
 
 		public:
 
@@ -54,14 +53,6 @@ namespace sx
 			if constexpr((IW-R*2) < (R*2+1)) static_assert(false, "Kernel does not fit in horizontal input space!");
 			if constexpr((IH-R*2) < (R*2+1)) static_assert(false, "Kernel does not fit in vertical input space!");
 			
-			this->OutTrans = simd::allocAligned<r32>(OW * OH);
-			if constexpr((F == Func::RELU) || (F == Func::PRELU)) this->OutReal = simd::allocAligned<r32>(OW * OH);
-			
-			this->Gradient = simd::allocAligned<r32>(IW * IH);;
-
-			this->Weights = simd::allocAligned<r32>(OW * OH * KER_AREA);
-			this->WeightsDlt = simd::allocAligned<r32>(OW * OH * KER_AREA);
-			
 			rng::rbuf(this->Weights, OW * OH * KER_AREA, _IntMin, _IntMax);
 		}
 
@@ -70,11 +61,6 @@ namespace sx
 		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		~OpLocal2 ( void ) final
 		{
-			simd::freeAligned<r32>(this->OutTrans);
-			if constexpr((F == Func::RELU) || (F == Func::PRELU)) simd::freeAligned<r32>(this->OutReal);
-			simd::freeAligned<r32>(this->Gradient);
-			simd::freeAligned<r32>(this->Weights);
-			simd::freeAligned<r32>(this->WeightsDlt);
 		}
 
 		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -108,7 +94,7 @@ namespace sx
 
 				for(auto ky = KER_RDX_MIN; ky != KER_RDX_MAX; ++ky) { for(auto kx = KER_RDX_MIN; kx != KER_RDX_MAX; ++kx)
 				{
-					Sum += this->Input[math::index_c(u64(ix+kx), u64(iy+ky), IW)] * this->Weights[math::index_c(o, w, KER_AREA)];
+					Sum += this->Input[math::index_c(u64(ix+kx), u64(iy+ky), IW)] * this->Weights[math::index_r(o, w, KER_AREA)];
 					++w;
 				}}
 
@@ -133,8 +119,8 @@ namespace sx
 		// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 		auto reset ( void ) -> void final
 		{
-			if(!this->IsLocked) std::memset(this->WeightsDlt, 0, OW * OH * KER_AREA * sizeof(r32));
-			std::memset(this->Gradient, 0, IW * IH * sizeof(r32));
+			if(!this->IsLocked) std::memset(this->WeightsDlt, 0, OW*OH*KER_AREA*sizeof(r32));
+			std::memset(this->Gradient, 0, IW*IH*sizeof(r32));
 
 			if(this->Front) this->Front->reset();
 		}
@@ -147,10 +133,9 @@ namespace sx
 			auto ox = u64(0);
 			auto oy = u64(0);
 			
-			for(auto iy = r32(R); iy < r32(IH - R); iy += IN_STEP_Y) { for(auto ix = r32(R); ix < r32(IW - R); ix += IN_STEP_X)
+			for(auto iy = r32(R); iy < r32(IH-R); iy += IN_STEP_Y) { for(auto ix = r32(R); ix < r32(IW-R); ix += IN_STEP_X)
 			{
 				const auto o = math::index_c(ox, oy, OW);
-					
 				auto w = u64(0);
 					
 				auto DerOut = r32(0.0f);
@@ -166,7 +151,7 @@ namespace sx
 				for(auto ky = KER_RDX_MIN; ky != KER_RDX_MAX; ++ky) { for(auto kx = KER_RDX_MIN; kx != KER_RDX_MAX; ++kx)
 				{
 					const auto i = math::index_c(u64(ix+kx), u64(iy+ky), IW);
-					const auto ow = math::index_c(o, w, KER_AREA);
+					const auto ow = math::index_r(o, w, KER_AREA);
 					
 					if(!this->IsLocked) this->WeightsDlt[ow] += (this->Input[i] * DerIn);
 					this->Gradient[i] += this->Weights[ow] * DerIn;
